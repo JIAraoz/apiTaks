2dd653af8f7e804b299b0aa24b716076ac3582d8
Nachvander <nachvander@gmail.com>
2024-07-08 08:12:33 -0300

    querys/params/body

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, Req } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('api/v1/task')
-export class TaskController {}
+export class TaskController {
+    @Get()
+    method(@Body() body:any){
+        return {body}
+    }
+    @Post(':id/:name')
+    method2(@Param() params:any){
+        return {params}
+    }
+    @Post('post')
+    method3(@Query() querys:any){
+        return {querys}
+    }
+}
