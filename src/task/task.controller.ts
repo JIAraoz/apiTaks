efde95e3ae516f4af0f58fbfc2cfbe61fc19cda9
Nachvander <nachvander@gmail.com>
2024-07-14 17:52:17 -0300

    feat-putTasks

--- src/task/task.controller.ts
+++ src/task/task.controller.ts
@@ -1,23 +1,30 @@
-import { Body, Controller, Get, Param, Post, Query, Req } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query, Req , Put} from '@nestjs/common';
 import { Request } from 'express';
 import { TaskDTO } from './dto/task.dto';
 import { TaskService } from './task.service';
+import { ITask } from './task.interface';
 
 @Controller('api/v1/task')
 export class TaskController {
     constructor(private readonly taskService:TaskService){}
-    @Get("/getById/:id")
-    getById(@Param() params:any){
-
-        return this.taskService.getById(params.id)
-    }
+    
     @Get()
     getAll(){
         return this.taskService.getAll()
     }
+    @Get(":id")
+    getById(@Param('id') id:string){
+
+        return this.taskService.getById(id)
+    }
+   
     @Post()
     create(@Body() taskDTO:TaskDTO){
         return this.taskService.create(taskDTO)
     }
-   
+    @Put(":id")
+    update(@Param("id") id:string,@Body() TaskDTO:TaskDTO):ITask{
+        return this.taskService.update(id , TaskDTO)
+    }
+  
 }
--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -6,7 +6,7 @@ import {v4 as uuidv4} from 'uuid'
 export class TaskService {
     tasks:ITask[] = []
     create(taskDTO:TaskDTO):ITask{
-        
+
         const task={
             id:uuidv4(),
             ...taskDTO
@@ -21,5 +21,11 @@ export class TaskService {
     getById(id:string):ITask{
         const task=this.tasks.find(e=>e.id===id)
         return task
+    }
+
+    update(id:string,TaskDTO:TaskDTO):ITask{
+       const newTask={id,...TaskDTO}
+       this.tasks = this.tasks.map((t) => (t.id === id ? newTask : t));
+        return newTask
     }
 } 
