2b78e30f6b804f98e4f7456092d7eb4fad4ec821
Nachvander <nachvander@gmail.com>
2024-07-23 19:14:52 -0300

    exeptionFilters

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { AllExeptionFilter } from './common/filters/http-exeption.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalFilters(new AllExeptionFilter());
   await app.listen(3000);
 }
 bootstrap();
+ 
