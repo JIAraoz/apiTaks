b4e104ca51e87ef38fb192b36b63e89c78e8e02e
Nachvander <nachvander@gmail.com>
2024-09-10 05:44:28 -0300

    img

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { AllExeptionFilter } from './common/filters/http-exeption.filter';
+import { TimeOutInterceptor } from './common/interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
   app.useGlobalFilters(new AllExeptionFilter());
+  app.useGlobalInterceptors(new TimeOutInterceptor())
   await app.listen(3000);
 }
 bootstrap();
